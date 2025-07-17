536ba82b2d3f7abe31b891e421bafd118d2a5f79
leblancja <leblancja@outlook.com>
2025-07-16 21:00:21 -0300

    initial commit

---
+++ rust/flake.nix
@@ -0,0 +1,37 @@
+{
+    description = "Basic rust environment";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+
+        rust-overlay.url = "github:oxalica/rust-overlay";
+    };
+
+    outputs = {
+        self,
+        nixpkgs,
+        flake-utils,
+        rust-overlay
+    }:
+        flake-utils.lib.eachDefaultSystem (system: let
+            pkgs = import nixpkgs {
+                inherit system;
+                overlays = [rust-overlay.overlays.default];
+            };
+            toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+        in {
+            devShells.default = pkgs.mkShell {
+                packages = [
+                    toolchain  
+                    pkgs.rust-analyzer-unwrapped
+                ];
+                RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+                
+                shellHook = ''
+                    echo "Rust environment active"
+                    echo "$(rustc --version)"
+                '';
+            };
+        });
+
+}
